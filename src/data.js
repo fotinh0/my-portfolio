c9f4e8265c28d766dbb3abcf15c43fd8cb75199b
Foti Ceci <foti.ceci@gmail.com>
2021-10-11 13:19:36 -0400

    Add random-quote-machine project

--- src/data.js
+++ src/data.js
@@ -8,6 +8,16 @@ export const projects = [
     image: "./keeper-app.gif",
     link: "https://keeper-app-fc.netlify.app/?title=gfh&content=hjk",
     github: "https://github.com/fotinh0/keeper-app"
+  },
+  {
+    title: "Random Quote Machine",
+    subtitle: "Random Quote Machine using React",
+    description:
+      "Need some motivation? Generate random quotes for inspiration to continue to grow and succeed.",
+    skills: "Built with: React",
+    image: "./random-quote-machine.gif",
+    link: "https://random-quote-machine-fc.netlify.app/",
+    github: "https://github.com/fotinh0/random-quote-machine"
   }
 ];
 
