cd47a0cf942578f351207aacc4e8e7e4bfef0ed0
Foti Ceci <foti.ceci@gmail.com>
2021-10-15 15:22:06 -0400

    add simon-game project

--- src/data.js
+++ src/data.js
@@ -4,8 +4,8 @@ export const projects = [
     subtitle: "React Clone of Google Keep",
     description:
       "Keep track of your notes and your todo's using the Keeper App. Inspired by the Google Keep application.",
-    skills: "Built with: React & CSS",
-    image: "./keeper-app.gif",
+    skills: "Built with: React",
+    image: "./gif-files/keeper-app.gif",
     link: "https://keeper-app-fc.netlify.app/?title=gfh&content=hjk",
     github: "https://github.com/fotinh0/keeper-app"
   },
@@ -15,9 +15,19 @@ export const projects = [
     description:
       "Need some motivation? Generate random quotes for inspiration to continue to grow and succeed.",
     skills: "Built with: React",
-    image: "./random-quote-machine.gif",
+    image: "./gif-files/random-quote-machine.gif",
     link: "https://random-quote-machine-fc.netlify.app/",
     github: "https://github.com/fotinh0/random-quote-machine"
+  },
+  {
+    title: "Simon Game",
+    subtitle: "Simon Game using jQuery",
+    description:
+      "Test your memory skills playing the Simon Game. Discover the strength of your memory now!",
+    skills: "Built with: HTML, CSS, jQuery",
+    image: "./gif-files/simon-game.gif",
+    link: "https://fotinh0.github.io/simon-game/",
+    github: "https://github.com/fotinh0/simon-game"
   }
 ];
 
