2a2aef6cf75acad4d8bc21e3891e306cfff0741e
Foti Ceci <foti.ceci@gmail.com>
2021-10-22 11:56:41 -0400

    update build folder

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,6 +7,7 @@ export default function Skills() {
     <section id="skills">
       <div className="container px-5 py-10 mx-auto">
         <div className="text-center mb-20">
+          {/* Skills Header */}
           <ChipIcon className="w-10 inline-block mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
             Skills &amp; Technologies
@@ -15,6 +16,8 @@ export default function Skills() {
             Skills and techonoligies I have implemented in previous projects.
           </p>
         </div>
+
+        {/* Skills List */}
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
           {skills.map((skill) => (
             <div key={skill} className="p-2 sm:w-1/2 w-full">
