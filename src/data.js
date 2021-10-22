23ff17b4403cbb550e5171978688fec93dd1ddb8
Foti Ceci <foti.ceci@gmail.com>
2021-10-22 11:52:21 -0400

    add markdown-preview project

--- src/data.js
+++ src/data.js
@@ -14,7 +14,7 @@ export const projects = [
     subtitle: "Random Quote Machine using React",
     description:
       "Need some motivation? Generate random quotes for inspiration to continue to grow and succeed.",
-    skills: "Built with: React",
+    skills: "Built with: React, CSS",
     image: "./gif-files/random-quote-machine.gif",
     link: "https://random-quote-machine-fc.netlify.app/",
     github: "https://github.com/fotinh0/random-quote-machine"
@@ -33,12 +33,23 @@ export const projects = [
     title: "FAQ Accordion Card",
     subtitle: "Responsive and Interactive FAQ Page",
     description:
-      "FAQ accordion card challenge from Frontend Mentor. User can hide/show the answer to a question when the question is clicked",
+      "FAQ accordion card challenge from Frontend Mentor. User can hide/show the answer to a question when the question is clicked.",
     skills: "Built with: HTML, CSS, JavaScript",
     image: "./gif-files/faq-card.gif",
     link: "https://frontend-mentor-faq-accordion-card.netlify.app/",
     github: "https://github.com/fotinh0/frontend-mentor-challenges/tree/master/faq-accordion-card-main"
+  },
+  {
+    title: "Markdown Previewer App",
+    subtitle: "Responsive Markdown Previewer using Marked Library",
+    description:
+      "Markdown Previewer App allows you to enter Github flavored markdown into the editor. Text is rendered as HTML as you type. Try it out for yourself now!",
+    skills: "Built with: React, CSS",
+    image: "./gif-files/markdown-preview.gif",
+    link: "markdown-previewer-fc.netlify.app/",
+    github: "https://github.com/fotinh0/markdown-preview-app"
   }
+  
 ];
 
 export const skills = [
